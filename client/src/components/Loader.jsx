eb8e1fea797cdab04bb2e0ebadb421e3cc4c2bdf
InsideAmber <lostinthoughtshard@gmail.com>
2023-03-11 23:44:38 +0530

    frontend and backend added

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { loader } from '../assets';
+
+const Loader = ({ noText }) => {
+  return (
+    <div className="fixed inset-0 z-10 h-screen bg-[rgba(0,0,0,0.7)] flex items-center justify-center flex-col">
+      <img
+        src={loader}
+        alt="loader"
+        className="w-[100px] h-[100px] object-contain "
+      />
+      {!noText && (
+        <p className="mt-[20px] font-epilogue font-bold text-[20px] text-center text-white">
+          Transaction is in progress <br />
+          Please wait...
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Loader;
